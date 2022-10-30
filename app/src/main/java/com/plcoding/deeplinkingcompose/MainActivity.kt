ab9713e496992cfa99524092998b4d27494eefa9
Philipp Lackner <mail@pl-coding.com>
2022-10-30 15:43:23 +0100

    Initial commit

---
+++ app/src/main/java/com/plcoding/deeplinkingcompose/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.plcoding.deeplinkingcompose
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
+import com.plcoding.deeplinkingcompose.ui.theme.DeeplinkingComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DeeplinkingComposeTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Button(onClick = {
+                                navController.navigate("detail")
+                            }) {
+                                Text(text = "To detail")
+                            }
+                        }
+                    }
+                    composable(
+                        route = "detail",
+                        deepLinks = listOf(
+                            navDeepLink {
+                                uriPattern = "https://pl-coding.com/{id}"
+                                action = Intent.ACTION_VIEW
+                            }
+                        ),
+                        arguments = listOf(
+                            navArgument("id") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) { entry ->
+                        val id = entry.arguments?.getInt("id")
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "The id is $id")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
